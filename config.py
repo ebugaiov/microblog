bfa1de48867de039fa4e6edbc18d51cb6705ede3
ebugaiov <evgeniy.bugaiov@gmail.com>
2025-02-10 18:36:13 +0100

    Pagination

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         f'sqlite:///{os.path.join(basedir, "app.db")}'
+    POSTS_PER_PAGE = 3
